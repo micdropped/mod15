853aade30bbf0d37173823a6dddaf409e0433356
daniel lukasik <daniel.lukasik10@gmail.com>
2020-02-16 21:14:07 +0100

    working search component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
 import PropTypes from 'prop-types';
-
+import Search from '../Search/SearchContainer';
 import List from '../List/ListContainer.js';
 //import { pageContents, listData } from '../../data/dataStore';
 
@@ -20,6 +20,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
